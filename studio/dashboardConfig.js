c5bfaf2d65b636cf89135576be2cfd4326cca5f9
Sanity.io <hello@sanity.io>
2020-11-16 13:26:05 +0530

    Initial commit

---
+++ studio/dashboardConfig.js
@@ -0,0 +1,53 @@
+export default {
+  widgets: [
+    {
+      name: 'sanity-tutorials',
+      options: {
+        templateRepoId: 'sanity-io/sanity-template-eleventy-blog'
+      }
+    },
+    {name: 'structure-menu'},
+    {
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              description:
+                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              sites: [
+                {
+                  buildHookId: '5fb230917cbead68aa9d2782',
+                  title: 'Sanity Studio',
+                  name: 'sanity-11-ty-orgsite-studio',
+                  apiId: '91eb4eeb-7692-4479-ad9c-4703f9adde3f'
+                },
+                {
+                  buildHookId: '5fb2309186dab35fcbb8c148',
+                  title: 'Blog Website',
+                  name: 'sanity-11-ty-orgsite',
+                  apiId: '1f17a95c-28b2-4fa2-a4b1-837ce3d4ff7a'
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/achanda101/sanity-11ty-orgsite',
+            category: 'Code'
+          },
+          {title: 'Frontend', value: 'https://sanity-11-ty-orgsite.netlify.app', category: 'apps'}
+        ]
+      }
+    },
+    {name: 'project-users', layout: {height: 'auto'}},
+    {
+      name: 'document-list',
+      options: {title: 'Recent blog posts', order: '_createdAt desc', types: ['post']},
+      layout: {width: 'medium'}
+    }
+  ]
+}
